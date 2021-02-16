aa366f287672b9ac3d804056d14b724b75a83359
billy <billy@localhost.localdomain>
2021-02-16 16:23:42 +1100

    Fix some parameters

--- CIFAR_main.py
+++ CIFAR_main.py
@@ -21,25 +21,25 @@ from models.conv_iResNet import conv_iResNet as iResNet
 from models.conv_iResNet import multiscale_conv_iResNet as multiscale_iResNet
 
 parser = argparse.ArgumentParser(description='Train i-ResNet/ResNet on Cifar')
-parser.add_argument('-densityEstimation', '--densityEstimation', dest='densityEstimation',
+parser.add_argument('-densityEstimation', '--densityEstimation', dest='densityEstimation', default=True,
                     action='store_true', help='perform density estimation')
 parser.add_argument('--optimizer', default="adamax", type=str, help="optimizer", choices=["adam", "adamax", "sgd"])
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.003, type=float, help='learning rate')
 parser.add_argument('--coeff', default=0.9, type=float, help='contraction coefficient for linear layers')
 parser.add_argument('--numTraceSamples', default=1, type=int, help='number of samples used for trace estimation')
-parser.add_argument('--numSeriesTerms', default=1, type=int, help='number of terms used in power series for matrix log')
+parser.add_argument('--numSeriesTerms', default=5, type=int, help='number of terms used in power series for matrix log')
 parser.add_argument('--powerIterSpectralNorm', default=5, type=int, help='number of power iterations used for spectral norm')
-parser.add_argument('--weight_decay', default=5e-4, type=float, help='coefficient for weight decay')
+parser.add_argument('--weight_decay', default=0., type=float, help='coefficient for weight decay')
 parser.add_argument('--drop_rate', default=0.1, type=float, help='dropout rate')
-parser.add_argument('--batch', default=128, type=int, help='batch size')
+parser.add_argument('--batch', default=64, type=int, help='batch size')
 parser.add_argument('--init_batch', default=1024, type=int, help='init batch size')
 parser.add_argument('--init_ds', default=2, type=int, help='initial downsampling')
-parser.add_argument('--warmup_epochs', default=10, type=int, help='epochs for warmup')
+parser.add_argument('--warmup_epochs', default=1, type=int, help='epochs for warmup')
 parser.add_argument('--inj_pad', default=0, type=int, help='initial inj padding')
 parser.add_argument('--epochs', default=200, type=int, help='number of epochs')
-parser.add_argument('--nBlocks', nargs='+', type=int, default=[4, 4, 4])
+parser.add_argument('--nBlocks', nargs='+', type=int, default=[16, 16, 16])
 parser.add_argument('--nStrides', nargs='+', type=int, default=[1, 2, 2])
-parser.add_argument('--nChannels', nargs='+', type=int, default=[16, 64, 256])
+parser.add_argument('--nChannels', nargs='+', type=int, default=[512, 512, 512])
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
@@ -52,7 +52,7 @@ parser.add_argument('-norm', '--norm', dest='norm', action='store_true',
                     help='compute norms of conv operators')
 parser.add_argument('-analysisTraceEst', '--analysisTraceEst', dest='analysisTraceEst', action='store_true',
                     help='analysis of trace estimation')
-parser.add_argument('-multiScale', '--multiScale', dest='multiScale', action='store_true',
+parser.add_argument('-multiScale', '--multiScale', dest='multiScale', action='store_true', default=True,
                     help='use multiscale')
 parser.add_argument('-fixedPrior', '--fixedPrior', dest='fixedPrior', action='store_true',
                     help='use fixed prior, default is learned prior')
@@ -66,7 +66,7 @@ parser.add_argument('--vis_server', default="localhost", type=str, help="server 
 parser.add_argument('--log_every', default=10, type=int, help='logs every x iters')
 parser.add_argument('-log_verbose', '--log_verbose', dest='log_verbose', action='store_true',
                     help='verbose logging: sigmas, max gradient')
-parser.add_argument('-deterministic', '--deterministic', dest='deterministic', action='store_true',
+parser.add_argument('-deterministic', '--deterministic', dest='deterministic', action='store_true', default=True,
                     help='fix random seeds and set cuda deterministic')
 
 
