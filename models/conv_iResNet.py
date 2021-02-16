9ed72bafdfb1898adf91b006d933797c1bc51a0e
billy <billy@localhost.localdomain>
2021-02-16 16:26:45 +1100

    Debug

--- models/conv_iResNet.py
+++ models/conv_iResNet.py
@@ -14,7 +14,7 @@ from .model_utils import squeeze as Squeeze
 from .model_utils import MaxMinGroup
 from spectral_norm_conv_inplace import spectral_norm_conv
 from spectral_norm_fc import spectral_norm_fc
-from matrix_utils import exact_matrix_logarithm_trace, power_series_matrix_logarithm_trace
+from matrix_utils import power_series_matrix_logarithm_trace
 
 from torch.distributions import constraints
 
