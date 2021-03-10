a8b0782177c4714533148651bc6a7348fd417206
billy <billy@localhost.localdomain>
2021-03-10 23:40:05 +1100

    debug inv attention dot 2

--- models/inv_attention.py
+++ models/inv_attention.py
@@ -139,9 +139,7 @@ class Attention_dot2(nn.Module):
         energy = torch.bmm(proj_query, proj_key)  # Batch matrix multiplication, [B, HW, HW]
         energy = self.nonlin(energy)
         with torch.no_grad():
-            energy_sum = torch.sum(energy, dim=(1,2))
-            print(energy_sum.size())
-            exit()
+            energy_sum = torch.sum(energy, dim=(1,2), keepdim=True)
         energy = energy / (2.0 * energy_sum)
         proj_value = self.value_conv(x).view(B, -1, H * W)  # [B, C, HW]
         out = torch.bmm(proj_value, energy).view(B, C, H, W)
