41522e432a70daa7f42fa53511992c81f7561ea5
billy <billy@localhost.localdomain>
2021-03-20 21:07:48 +1100

    Debug inv attention, add choice for convGamma, and refurbish residual branch

--- models/inv_attention.py
+++ models/inv_attention.py
@@ -9,7 +9,7 @@ class Attention_concat(nn.Module):
       Concatenation Style PAM, with turbulance
     '''
 
-    def __init__(self, in_c, k=4):
+    def __init__(self, in_c, k=4, convGamma = False):
         super(Attention_concat, self).__init__()
         self.in_c = in_c
         self.inter_c = in_c // k
@@ -17,10 +17,16 @@ class Attention_concat(nn.Module):
         self.key_conv = nn.Conv2d(in_channels=in_c, out_channels=self.inter_c, kernel_size=1)
         self.concat_conv = nn.Conv2d(in_channels=self.inter_c * 2, out_channels=1, kernel_size=1, bias=False)
         self.value_conv = spectral_norm_fc(nn.Conv2d(in_channels=in_c, out_channels=in_c, kernel_size=1),
-                                           coeff=.7, n_power_iterations=5)
-        self.gamma = Parameter(torch.zeros(1))
+                                           coeff=.9, n_power_iterations=5)
+        self.convGamma = convGamma
+        if convGamma:
+            self.gamma = spectral_norm_fc(nn.Conv2d(in_channels=in_c, out_channels=in_c, kernel_size=1),
+                                           coeff=.9, n_power_iterations=5)
+        else:
+            self.gamma = Parameter(torch.zeros(1))
+            self.nonlin_2 = nn.Tanh()
         self.nonlin_1 = nn.ELU()
-        self.nonlin_2 = nn.Tanh()
+
 
     def forward(self, x):
         B, C, H, W = x.size()
@@ -30,31 +36,24 @@ class Attention_concat(nn.Module):
         proj_key = proj_key.repeat(1, 1, H * W, 1)
         concat_feature = torch.cat([proj_query, proj_key], dim=1)  # [B, 2*inter_c, HW, HW]
         energy = self.concat_conv(concat_feature).squeeze()  # [B,  HW, HW]
+        energy = self.nonlin_1(energy)
         energy = energy / (1.5 * torch.sum(energy, dim=(1,2), keepdim=True))
         proj_value = self.value_conv(x).view(B, -1, H * W)
         out = torch.bmm(proj_value, energy).view(B, -1, H, W)
-        tmp = self.nonlin_2(self.gamma)
-        out = tmp * out + x
+        if self.convGamma:
+             out = self.gamma(out)
+        else:
+            tmp = self.nonlin_2(self.gamma)
+            out = tmp * out
         return out
 
 
-def turbulance_hook(module, inputs):
-    with torch.no_grad():
-        x = inputs[0]
-        res = module.forward(x)
-        turbu_res = module.forward(x * 1.0000001)
-        lip = torch.dist(turbu_res, res) / torch.dist(x, x * 1.0000001)
-        if lip > 0.9:
-            module.gamma *=(0.9 / lip)
-        else:
-            pass
 
 
 class InvAttention_concat(nn.Module):
-    def __init__(self, in_c, k=4, numTraceSamples = 1, numSeriesTerms = 5):
+    def __init__(self, in_c, k=4, numTraceSamples = 1, numSeriesTerms = 5, convGamma=False):
         super(InvAttention_concat, self).__init__()
-        self.res_branch = Attention_concat(in_c, k=k)
-        self.res_branch.register_forward_pre_hook(turbulance_hook)
+        self.res_branch = Attention_concat(in_c, k=k, convGamma=convGamma)
         self.numTraceSamples = numTraceSamples
         self.numSeriesTerms = numSeriesTerms
     def forward(self, x, ignore_logdet=False):
@@ -128,7 +127,7 @@ class Attention_dot2(nn.Module):
     '''
     Dot product, inv
     '''
-    def __init__(self, input_channel_num, k=4):
+    def __init__(self, input_channel_num, k=4, convGamma=False):
         super(Attention_dot2, self).__init__()
         self.c_in = input_channel_num
         self.query_conv = nn.Conv2d(in_channels=self.c_in, out_channels=self.c_in // k, kernel_size=1)
@@ -136,8 +135,14 @@ class Attention_dot2(nn.Module):
         self.value_conv = spectral_norm_fc(nn.Conv2d(in_channels=self.c_in, out_channels=self.c_in, kernel_size=1),
                                            coeff=.9, n_power_iterations=5)
         self.nonlin = nn.ELU()
-        self.gamma = spectral_norm_fc(nn.Conv2d(in_channels=self.c_in, out_channels=self.c_in, kernel_size=1),
-                                           coeff=.9, n_power_iterations=5)
+        self.convGamma = convGamma
+        if convGamma:
+            self.gamma = spectral_norm_fc(nn.Conv2d(in_channels=self.c_in, out_channels=self.c_in, kernel_size=1),
+                                             coeff=.9, n_power_iterations=5)
+        else:
+            self.gamma = Parameter(torch.zeros(1))
+            self.nonlin_2 = nn.Tanh()
+
     def forward(self, x):
         B, C, H, W = x.size()
         proj_query = self.query_conv(x).view(B, -1, H * W).permute(0, 2, 1)  # [B, HW, C//8]
@@ -148,8 +153,10 @@ class Attention_dot2(nn.Module):
         energy = energy / (1.5 * energy_sum) #hooray
         proj_value = self.value_conv(x).view(B, -1, H * W)  # [B, C, HW]
         out = torch.bmm(proj_value, energy).view(B, C, H, W)
-        out = self.gamma(out)
-        out = out + x
+        if self.convGamma:
+            out = self.gamma(out)
+        else:
+            out = self.nonlin_2(self.gamma) * out
         return out
 
 class InvAttention_dot2(nn.Module):
