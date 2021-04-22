7062edb7ed8425da435df07b45fc2252bdf64778
billy <billy@localhost.localdomain>
2021-04-22 17:49:52 +1000

    celebA resize to 32, debug

--- attention_test.py
+++ attention_test.py
@@ -179,8 +179,8 @@ def main():
         train_subset = torch.utils.data.Subset(trainset, list(range(1000)))
         test_subset = torch.utils.data.Subset(testset, list(range(1000)))
     elif args.dataset == 'celebA':
-        resize_tran = [transforms.Resize(32)]
-        transform_train = transforms.Compose(transform_train +  resize_tran)
+        resize_chain = [transforms.Resize(32)]
+        transform_train = transforms.Compose(train_chain + dens_est_chain +  resize_chain)
         dataset = CelebADataset('./data', transform=transform_train)
         length = len(dataset)
         train_subset = torch.utils.data.Subset(dataset, list(range(1000)))
