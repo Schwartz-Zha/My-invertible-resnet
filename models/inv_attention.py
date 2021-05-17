cbcd4693febfc1036eb9afd8cf8adad32b920233
BillySchwartz <billy@xiangli.gpu>
2021-05-17 18:51:01 +1000

    good dot and concat inv_attention for generation

--- models/inv_attention.py
+++ models/inv_attention.py
@@ -198,7 +198,7 @@ class Attention_concat(nn.Module):
         self.inter_c = in_c // k
         self.query_conv = nn.Conv2d(in_channels=self.in_c, out_channels=self.in_c // k, kernel_size=1)
         self.key_conv = nn.Conv2d(in_channels=self.in_c, out_channels=self.in_c // k, kernel_size=1)
-        self.concat_conv = nn.Conv2d(in_channels=self.inter_c * 2, out_channels=1, kernel_size=1, bias=False)
+        self.concat_conv = nn.Conv2d(in_channels=self.inter_c * 2, out_channels=1, kernel_size=1)
         self.value_conv = spectral_norm_fc(nn.Conv2d(in_channels=in_c, out_channels=in_c, kernel_size=1),
                                            coeff=.9, n_power_iterations=5)
         self.convGamma = convGamma
